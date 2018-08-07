7ac20f4c2bad6ab81bc0398f429f4cbf06e7293d
Brian Mejia <Bmejia319@gmail.com>
2018-08-07 12:07:18 -0400

    latest updates

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -13,12 +13,50 @@ $(document).ready(function() {
     loadVids();
 
     function loadVids() {
-        // $.getJSON(URL, options, playlistId, function(data) {
-        // console.log(data);
-        // })
-
         $.getJSON(URL, options, function(data) {
             console.log(data);
-        })
+            var id = data.items[0].snippet.resourceId.videoId;
+            ytPlayer(id);
+            resultsLoop(data);
+        });
     } 
-});
+
+    function ytPlayer(id) {
+        $(".player").html(`
+        <iframe width="560" 
+            height="315" 
+            src="https://www.youtube.com/embed/${id}" 
+            frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>
+        </iframe>`);
+    }
+
+    function resultsLoop(data) {
+        $.each(data.items, function (i, item) {
+
+            var thumb = item.snippet.thumbnails.medium.url;
+            var title = item.snippet.title;
+            var description = item.snippet.description.substring(0, 150);
+            var vid = item.snippet.resourceId.videoId;
+
+            $(".part-two").append(`
+            <article class="vids" data-key="${vid}">
+
+                <img src="${thumb}" 
+                alt="thumbnail" 
+                class="thumb">
+    
+            <div class="text">
+                <h4 class="title">${title}</h4>
+                <p class="description">${description}</p>
+            </div>
+            </article>`
+            );
+         }); 
+      }
+
+        $(".part-two").on("click", 'article', function () {
+            var newId = $(this).attr("data-key");
+            console.log(newId);
+            ytPlayer(id);
+        });
+    });
